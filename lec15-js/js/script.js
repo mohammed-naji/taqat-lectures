5e72665ee3b84b0d4fc754fd8c90cd1050751d0e
Mohammed Naji <m.naji1512@gmail.com>
2026-04-28 14:02:58 +0300

    website

---
+++ lec15-js/js/script.js
@@ -0,0 +1,8 @@
+// DOM => Document Object Model
+// BOM => Browser Object Model
+const mins_btns = document.querySelectorAll(".mins");
+const plus_btns = document.querySelectorAll(".plus");
+
+for (let i = 0; i < mins_btns.length; i++) {}
+
+// console.log(mins_btns, plus_btns);
