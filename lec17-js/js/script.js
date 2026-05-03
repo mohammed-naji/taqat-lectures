81c4d0160959a666fc59a1f41949f6aa059d74c4
Mohammed Naji <m.naji1512@gmail.com>
2026-05-03 16:20:44 +0300

    lec 17 - js events

---
+++ lec17-js/js/script.js
@@ -0,0 +1,88 @@
+// // console.log(age);
+// // let age = 20;
+// // console.log(age);
+
+// console.log(b());
+// function b() {
+//   function a() {
+//     return 0;
+//   }
+//   return a();
+//   function a() {
+//     return 1;
+//   }
+//   function a() {
+//     return 10;
+//   }
+// }
+// console.log(b());
+
+// window
+// document
+
+// DOM => Document Object Model
+// BOM => Browser Object Model
+
+// selector . event = action
+// document.oncopy = function () {
+//   alert("ظلموك فقالو الجي اس صعبة!");
+//   return false;
+// };
+
+// document.onselectstart = function () {
+//   return false;
+// };
+
+// document.onclick = () => {};
+
+// document.onclick = function () {};
+
+// document.addEventListener("click", sofyan);
+
+// function sofyan() {
+//   console.log("Done");
+// }
+
+// let name = prompt("Enter your name");
+// let age = prompt("Enter your age");
+// console.log(name, age);
+
+// if (name.toLowerCase().includes("a") && name.toLowerCase().includes("z")) {
+//   alert("doneee");
+// }
+
+// name.include
+
+// if (age >= 18) {
+//   alert("أهلا بك في المصفوعين");
+// } else {
+//   alert("هو انت قد المصفوعين!!!!");
+// }
+
+// selector => button
+// event => click
+// action => hide text
+
+// document.getElementById("hide").addEventListener("click", function () {
+//   document.getElementById("txt").style.display = "none";
+// });
+// document.getElementById("show").addEventListener("click", function () {
+//   document.getElementById("txt").style.display = "block";
+// });
+
+// var both = document.getElementById("both");
+// var txt = document.getElementById("txt");
+
+// both.addEventListener("click", function () {
+//   if (both.innerHTML == "Hide Text") {
+//     txt.style.display = "none";
+//     both.innerHTML = "Show Text";
+//   } else {
+//     txt.style.display = "block";
+//     both.innerHTML = "Hide Text";
+//   }
+// });
+
+// if (5 === "5") {
+//   console.log("Done");
+// }
